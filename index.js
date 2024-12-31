55886b9573f1b0adae9397661cb3b22a1fecf4a2
Matthy <paulmatthy900@gmail.com>
2024-12-31 01:38:46 +0100

    ExploreWorld

---
+++ index.js
@@ -0,0 +1,14 @@
+// const explore = document.querySelector('.explore-img')
+
+// gsap.set(explore, {opacity: 0, y: 20})
+// gsap.set(
+//     ScrollTrigger.create({
+//         trigger: '.explore',
+//         start: 'top center',
+//         end: 'bottom center',
+//         markers: true,
+//         onEnter: () => {
+//             gsap.set(explore, {opacity: 1, delay: 0.2, y: 0})
+//         },
+//     })
+// )
